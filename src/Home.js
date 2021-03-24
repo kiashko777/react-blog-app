01bfa433d8e57accae44f898be38f8be53a845ab
kiashko777 <kiashko777@gmail.com>
2021-03-24 10:19:02 +0400

    Add all files

---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import BlogList from "./BlogList";
+import useFetch from './useFetch';
+
+const Home = () => {
+	const {data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
+
+	return (
+		<div className="home">
+			{error && <div>{error}</div>}
+			{isLoading && <p>Loading...</p>}
+			{blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+			{/* <BlogList
+        blogs={blogs.filter(blog => blog.author === "Mario")}
+        title="Mario Blogs"
+      /> */}
+		</div>
+	);
+};
+
+export default Home;
